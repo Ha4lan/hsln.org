a3c480612dc913cf11ff6be8cb99274ab7c44ae8
Ha4lan <kaaiontop@gmail.com>
2024-08-05 14:01:24 +0900

    astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'shark-gradient': 'linear-gradient(135deg, #e0f7fa, #b2ebf2, #80deea, #4dd0e1)',
+			  },
+			  colors: {
+				'light-blue': '#e0f7fa',
+				'circle-blue': 'rgba(0, 174, 239, 0.3)',
+			  },
+		},
 	},
 	plugins: [],
 }
